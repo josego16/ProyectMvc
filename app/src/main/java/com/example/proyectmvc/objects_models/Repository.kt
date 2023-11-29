0a88972622d5297b3acadc6b49374a491c84318f
josego16 <jgomlin@g.educaand,es>
2023-11-29 19:36:21 +0100

    hemos terminado la parte de recyclerView y cardview

--- app/src/main/java/com/example/proyectmvc/objects_models/Repository.kt
+++ app/src/main/java/com/example/proyectmvc/objects_models/Repository.kt
@@ -2,14 +2,14 @@ package com.example.proyectmvc.objects_models
 
 import com.example.proyectmvc.models.coche
 
-object repository {
+object Repository {
     val listCoches: List<coche> = listOf(
         coche(
             "Mercedes-AMG",
             "Mercedes",
             "Mercedes-AMG",
             "+49 123456789",
-            "https://images.app.goo.gl/XLxwFFmLeW71VSKq5"
+            "https://urlzs.com/XYanF"
         ),
 
         coche(
@@ -17,7 +17,63 @@ object repository {
             "Red Bull",
             "Red Bull Racing",
             "+43 987654321",
-            "https://images.app.goo.gl/TqtEqFzBpw1zXb6U8"
+            "https://urlzs.com/8zPjy"
+        ),
+
+        coche(
+            "Scuderia Ferrari",
+            "Ferrari",
+            "SF21",
+            "+39 0123456789",
+            "https://urlzs.com/SZXqr"
+        ),
+
+        coche(
+            "McLaren F1 Team",
+            "McLaren",
+            "MCL35M",
+            "+44 987654321",
+            "https://urlzs.com/x6Q9w"
+        ),
+
+        coche(
+            "Alpine F1 Team",
+            "Alpine",
+            "A521",
+            "+33 987654321",
+            "https://urlzs.com/yuvCC"
+        ),
+
+        coche(
+            "Scuderia AlphaTauri",
+            "AlphaTauri",
+            "AT02",
+            "+43 123456789",
+            "https://urlzs.com/1J4m2"
+        ),
+
+        coche(
+            "Aston Martin F1 Team",
+            "Aston Martin",
+            "AMR21",
+            "+44 123456789",
+            "https://urlzs.com/4MykY"
+        ),
+
+        coche(
+            "Haas F1 Team",
+            "Haas",
+            "VF-21",
+            "+1 987654321",
+            "https://urlzs.com/WPwPj"
+        ),
+
+        coche(
+            "Williams Racing",
+            "Williams",
+            "FW43B",
+            "+44 987654321",
+            "https://urlzs.com/aFi21"
         )
     )
 }
